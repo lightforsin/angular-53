6ab04e2c4a2ab6eb41efb24e23b3528e1a10af04
PaulBordea <lightforsin@yahoo.com>
2017-04-21 14:33:37 +0300

    Extract the Article model. Add several articles. Add sorting.

---
+++ src/app/article/article.model.ts
@@ -0,0 +1,31 @@
+export class Article {
+  title: string
+  link: string
+  votes: number
+
+  constructor(title: string, link: string, votes?: number) {
+      this.title = title;
+      this.link = link;
+      this.votes = votes;
+  }
+
+
+  voteUp() {
+    this.votes += 1;
+    return false;
+  }
+
+  voteDown() {
+    this.votes -= 1;
+    return false;
+  }
+
+  domain(): string {
+    try {
+      const link: string = this.link.split('//')[1];
+      return link.split('/')[0];
+    } catch (err) {
+      return null;
+    }
+  }
+}
